63a35077297c335b9347ea5e025327b44a583cc8
Shouvik Chatterjee <58618873+techieshouvik@users.noreply.github.com>
2022-09-30 04:34:34 +0530

    port changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 const recs = require('./resources/bcawebtech.json')
 
 app.set("view engine", "ejs");
